44aefd322722900582b8147821ba94e0504337e9
Eldon Ahrold <eahrold@bigclubdigital.com>
2019-07-29 15:17:49 -0500

    Update How Icon is handled, switch from base64 to png url.
    - Refactor Config, add validation to config JSON

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 const fs = require('fs');
-var iconFileBuffer = fs.readFileSync('./data/icon.png');
-var icon = iconFileBuffer.toString('base64');
 
 const http = require('http');
 const path = require('path');
@@ -14,12 +12,52 @@ app.use(bodyParser.text({type: 'text'}));
 //Express Static File Serving
 app.use(express.static('data'));
 
-var infoFileContents = require('./data/info.json');
-infoFileContents.icon = icon;
+// Load info.json data
+const infoFileContents = require('./data/info.json');
+
+const resolveHost = (request) => {
+    return request.headers['x-forwarded-host'] || request.get('host')
+}
+
+class InfoConfigError extends Error {
+    constructor(message) {
+        super(`[Info configuration error] ${message}`)
+    }
+}
+
+class InfoConfig {
+    constructor({ title, summary, website, icon }) {
+        Object.assign(this, { title, summary, website, icon })
+    }
+
+    validate() {
+        const { title, summary, website, icon } = this
+        if(typeof title !== 'string' || !title.length) throw new InfoConfigError("title is required, and must be a string")
+        if(typeof summary !== 'string' || !summary.length) throw new InfoConfigError("summary is required, and must be a string")
+        if(typeof website !== 'string' || !website.length) throw new InfoConfigError("website is required, and must be a string")
+        if(!website.startsWith('https')) throw new InfoConfigError("the website must be served over HTTPS")
+
+        // If the icon has been declared in the JSON, it must also be served over HTTPS
+        if(typeof icon === 'string' && !icon.startsWith("https")) throw new InfoConfigError("the custom icon must be served over HTTPS")
+    }
+
+    resolveIcon(request) {
+        // If custom icon file has been defined use that.
+        // otherwise use the default icon.png file
+        return this.icon || `https://${resolveHost(request)}/icon.png`;
+    }
+}
+
+const infoConfig = new InfoConfig(infoFileContents)
+
+// Validate the config data
+// If it's not in proper order this will throw.
+infoConfig.validate()
 
 app.use('/info', function(req, res, next)
 {
-    res.status(200).json(infoFileContents);
+    var icon = infoConfig.resolveIcon(req)
+    res.status(200).json({...infoConfig, icon});
 });
 
 app.use('/health', function(req, res, next)
